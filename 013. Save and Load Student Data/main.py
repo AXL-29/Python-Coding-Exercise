234a6c55d8bcbc0f1f30ea5a52cb6519df624ba0
AXL-29 <gimpaojade4@gmail.com>
2025-12-08 02:55:41 +0800

    Add initial implementation of student grade file saving coding exercise

---
+++ 013. Save and Load Student Data/main.py
@@ -0,0 +1,46 @@
+def get_valid_int(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value < 0:
+                print("Please enter a positive number, kindly try again.")
+                continue
+            return value
+        except ValueError:
+            print("Please enter a valid number, kindly try again.")
+
+
+def get_valid_float(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value < 1 or value > 100:
+                print("Please enter a valid numeric value between 1 - 100, kindly try again.")
+                continue
+            return value
+        except ValueError:
+            print("Please enter a valid numeric value, kindly try again.")
+
+
+def validate_name(prompt):
+    while True:
+        value = input(prompt).strip().lower()
+        if value and value.replace(" ", "").isalpha():
+            return value
+        else:
+            print("Invalid input, letters and spaces only.")
+
+grades = {"Jade": (98, "A")}
+
+with open("grade.txt", "a") as file:
+    for name, (score, grade) in grades.items():
+        file.write(f"{name}: {score} : {grade}\n")
+
+    count = get_valid_int("Enter a number: ")
+    for i in range(count):
+        name = validate_name("Enter a name: ")
+        score = get_valid_float("Enter a score: ")
+        grade = input("Enter a grade: ")
+
+        grades["name"] = (score, grade)
+
