897d217ed0c0fbac7b8bcddc4b082c40f2facc62
Jade <gimpaojade4@gmail.com>
2025-12-19 04:22:19 +0800

    Fix car speed scaling and spawning logic

--- 014. Turtle Crossing Game/car_manager.py
+++ 014. Turtle Crossing Game/car_manager.py
@@ -14,24 +14,30 @@ class CarManager(Turtle):
         self.hideturtle()
         self.cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
-    
+        self.spawn_counter = 0
+
     def create_car(self):
         new_car = Turtle("square")
         new_car.color(random.choice(COLORS))
         new_car.shapesize(stretch_wid=1, stretch_len=2)
         new_car.penup()
-        new_car.goto(320, random.randint(-260, 280))
+        new_car.goto(320, random.randint(-240, 280))
         new_car.setheading(180)
         self.cars.append(new_car)
     
     def spawn_car(self):
-        if random.randint(1, SPAWN_CHANCE) == 1:
+        self.spawn_counter += 1
+        if self.spawn_counter >= SPAWN_CHANCE:
             self.create_car()
+            self.spawn_counter = 0
 
     def move_cars(self):
-        for car in self.cars:
+        for car in self.cars[:]:  # iterate over a copy
             car.forward(self.car_speed)
             if car.xcor() < -320:
                 car.hideturtle()
                 self.cars.remove(car)
-        
+
+    
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
